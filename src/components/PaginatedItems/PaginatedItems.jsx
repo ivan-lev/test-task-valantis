8dbe1324fe3388dc3d536066cf5a0e4b73b651e7
Ivan Lev <fanatos@mail.ru>
2024-03-08 19:46:32 +0500

    style: don't show pagination if the items count is less than max displayed on the page

--- src/components/PaginatedItems/PaginatedItems.jsx
+++ src/components/PaginatedItems/PaginatedItems.jsx
@@ -3,20 +3,24 @@ import './PaginatedItems.css';
 import ReactPaginate from 'react-paginate';
 import React, { useEffect, useState } from 'react';
 
-// import CardList from '../CardList/CardList';
-
-export default function PaginatedItems({ itemsPerPage, itemsList, setListToDisplay }) {
+export default function PaginatedItems({
+  itemOffset,
+  setItemOffset,
+  itemsPerPage,
+  itemsList,
+  setPaginatedList
+}) {
   // We provide itemsList and start work with it.
   const [pageCount, setPageCount] = useState(0);
   // Here we use item offsets; we could also use page offsets
   // following the API or data you're working with.
-  const [itemOffset, setItemOffset] = useState(0);
+  // const [itemOffset, setItemOffset] = useState(0);
 
   useEffect(() => {
     // Fetch items from another resources.
     const endOffset = itemOffset + itemsPerPage;
     // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    setListToDisplay(itemsList.slice(itemOffset, endOffset));
+    setPaginatedList(itemsList.slice(itemOffset, endOffset));
     // setCurrentItems(itemsList.slice(itemOffset, endOffset));
     setPageCount(Math.ceil(itemsList.length / itemsPerPage));
   }, [itemOffset, itemsPerPage, itemsList]);
@@ -28,35 +32,30 @@ export default function PaginatedItems({ itemsPerPage, itemsList, setListToDispl
     setItemOffset(newOffset);
   };
 
-  // console.log('currentItems:', currentItems);
-
   return (
     <>
-      {/* <Items currentItems={currentItems} /> */}
-      {/* {currentItems.length !== 0 && <CardList list={currentItems} />} */}
-      <ReactPaginate
-        nextLabel="вперёд"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={3}
-        marginPagesDisplayed={2}
-        pageCount={pageCount}
-        previousLabel="назад"
-        pageClassName="pagination__item"
-        pageLinkClassName="pagination__link"
-        previousClassName="pagination__item"
-        previousLinkClassName="pagination__previous"
-        nextClassName="pagination__item"
-        nextLinkClassName="pagination__next"
-        breakLabel="..."
-        breakClassName="pagination__item"
-        breakLinkClassName="pagination__link"
-        containerClassName="pagination"
-        activeClassName="pagination__active"
-        renderOnZeroPageCount={null}
-      />
+      {itemsList.length > itemsPerPage && (
+        <ReactPaginate
+          nextLabel="вперёд"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="назад"
+          pageClassName="pagination__item"
+          pageLinkClassName="pagination__link"
+          previousClassName="pagination__item"
+          previousLinkClassName="pagination__previous"
+          nextClassName="pagination__item"
+          nextLinkClassName="pagination__next"
+          breakLabel="..."
+          breakClassName="pagination__item"
+          breakLinkClassName="pagination__link"
+          containerClassName="pagination"
+          activeClassName="pagination__active"
+          renderOnZeroPageCount={null}
+        />
+      )}
     </>
   );
 }
-
-// Add a <div id="container"> to your HTML to see the componend rendered.
-// ReactDOM.render(<PaginatedItems itemsPerPage={4} />, document.getElementById('container'));
