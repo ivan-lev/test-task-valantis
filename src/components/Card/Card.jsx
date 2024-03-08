d151c4303a11f1af3758a7dbc70f293553f5997a
Ivan Lev <fanatos@mail.ru>
2024-03-08 19:47:07 +0500

    style: minor layout change

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,10 +9,8 @@ export default function Card({ data }) {
       {data.brand && <span className="card__brand">{data.brand}</span>}
 
       <div className="card__info">
-        <div className="card__top-row">
-          <span className="card__id">{data.id}</span>
-          <span className="card__price">{data.price}</span>
-        </div>
+        <span className="card__id">{data.id}</span>
+        <span className="card__price">{data.price}</span>
         <span className="card__title">{data.product}</span>
       </div>
     </div>
