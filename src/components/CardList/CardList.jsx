e08f56ed4e807df1028ac7e2697d37fc0dd1679e
Ivan Lev <fanatos@mail.ru>
2024-03-08 19:47:45 +0500

    refactor: change variable name

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -3,11 +3,11 @@ import './CardList.css';
 import React from 'react';
 import Card from '../Card/Card';
 
-export default function CardList({ listToDisplay }) {
+export default function CardList({ paginatedList }) {
   return (
     <>
       <ul className="card-list">
-        {listToDisplay.map(item => (
+        {paginatedList.map(item => (
           <li className="card-list__item" key={item.id}>
             <Card data={item} />
           </li>
