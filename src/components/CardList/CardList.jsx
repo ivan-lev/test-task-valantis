a1c834b1d3e9a65876d5ac8180801b76490608ba
Ivan Lev <fanatos@mail.ru>
2024-03-08 11:12:13 +0500

    style: some minor changes in layout

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,14 +1,15 @@
 import './CardList.css';
 
 import React from 'react';
+import Card from '../Card/Card';
 
 export default function CardList({ list }) {
   return (
     <>
-      <ul className="movies-card-list">
+      <ul className="card-list">
         {list.map(item => (
-          <li className="card-list__item" key={item}>
-            {item}
+          <li className="card-list__item" key={item.id}>
+            <Card data={item} />
           </li>
         ))}
       </ul>
